cae38ad9a9fbdd86382359e1063d7f9bb9870bc3
thijsBoet <m.boet@chello.nl>
2022-11-18 16:50:01 +0100

    build first fetch function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hi there!');
+var mykey = config.OMDBAPI;
+
+const fetchData = async () => {
+	const response = await axios.get(`http://www.omdbapi.com/`, {
+		params: {
+			apikey: mykey,
+			s: 'avengers',
+		},
+	});
+	console.log(response.data);
+	const data = await response.json();
+	return data;
+};
+
+fetchData();
