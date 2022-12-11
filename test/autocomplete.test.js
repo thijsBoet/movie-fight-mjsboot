a0a8716a2b91fa3c108c2b6666c51b213a7e5098
thijsBoet <m.boet@chello.nl>
2022-12-11 19:14:01 +0100

    finished testing

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,6 +1,22 @@
-beforeEach(() => {
-    document.querySelector('#target').innerHTML = '';
+const waitFor = selector => {
+	return new Promise((resolve, reject) => {
+		const interval = setInterval(() => {
+			if (document.querySelector(selector)) {
+				clearInterval(interval);
+				clearTimeout(timeout);
+				resolve();
+			}
+		}, 30);
 
+		const timeout = setTimeout(() => {
+			clearInterval(interval);
+			reject();
+		}, 2000);
+	});
+};
+
+beforeEach(() => {
+	document.querySelector('#target').innerHTML = '';
 	createAutoComplete({
 		root: document.querySelector('#target'),
 		fetchData() {
@@ -18,16 +34,28 @@ beforeEach(() => {
 
 it('Dropdown starts closed', () => {
 	const dropdown = document.querySelector('.dropdown');
+
 	expect(dropdown.className).not.to.include('is-active');
 });
 
 it('After searching, dropdown opens up', async () => {
-    const input = document.querySelector('input');
-    input.value = 'avengers';
-    input.dispatchEvent(new Event('input'));
+	const input = document.querySelector('input');
+	input.value = 'avengers';
+	input.dispatchEvent(new Event('input'));
+
+	await waitFor('.dropdown-item');
+
+	const dropdown = document.querySelector('.dropdown');
+	expect(dropdown.className).to.include('is-active');
+});
+
+it('After searching, displays some results', async () => {
+	const input = document.querySelector('input');
+	input.value = 'avengers';
+	input.dispatchEvent(new Event('input'));
 
     await waitFor('.dropdown-item');
 
-    const dropdown = document.querySelector('.dropdown');
-    expect(dropdown.className).to.include('is-active');
+    const items = document.querySelectorAll('.dropdown-item');
+    expect(items.length).to.equal(3);
 });
